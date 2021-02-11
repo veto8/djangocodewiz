6f894b5b201d67b0e7d48633d70e6cd723779618
codewiz <c.chaiwat.ch@gmail.com>
2021-02-11 07:46:34 +0700

    init project
    Signed-off-by: codewiz <c.chaiwat.ch@gmail.com>

---
+++ README.md
@@ -0,0 +1,37 @@
+# How to install Django
+
+`pip install django`
+
+# How django create project
+
+`django-admin startproject <projectname>`
+
+# How django create app
+`python manage.py startapp <appname>`
+# How django create admin
+
+## Need to migrate for let django know
+
+`python manage.py migrate`
+
+## Then create super user
+
+`python manage.py createsuperuser`
+
+# User for test
+## user: evadaisie
+    pass: eve555%%
+
+## user: Newuser3
+    pass: passpass55
+
+# How migration?
+* After you create model Post (for example)
+** then `python manage.py makemigrations`
+** to build migrations file.
+* Next is you need django create database from migrations file for you
+** just type `python manage.py sqlmigrate blog 0001 (blog = project name, 0001 is migration file)
+## What's migration?
+### migration cmd for prepare every thing like a create Post model
+### Cart model, it generate file under migrations folder
+### migrate cmd is execute every thing from migration.
---
+++ dj_project/users/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .form import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            info_msg = 'Your account has been created! you are now able to log in'
+            messages.success(request, info_msg)
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+
+    return render(request, 'users/register.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(
+            request.POST, request.FILES, instance=request.user.profile
+        )
+        
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+
+            info_msg = 'Your account has been updated!'
+            messages.success(request, info_msg)
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+
+    return render(request, 'users/profile.html', context)
