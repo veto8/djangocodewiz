a1bfb9365945adc82bb37d6dbe73a2791ad172c4
codewiz <c.chaiwat.ch@gmail.com>
2021-02-13 17:42:23 +0700

    update: nav bar
    Signed-off-by: codewiz <c.chaiwat.ch@gmail.com>

--- dj_project/users/models.py
+++ dj_project/users/models.py
@@ -2,15 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
 
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
-    def __str__(self) -> str:
+    def __str__(self):
         return f'{self.user.username} Profile'
 
-    def save(self):
-        super().save()
+    def save(self, *args, **kwarges):
+        super().save(*args, **kwarges)
 
         img = Image.open(self.image.path)
 
